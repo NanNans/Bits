86d099f531f64bb6a3a1a7e70b7148bea896fee8
NanNans <hannahpearlherman@gmail.com>
2014-03-05 12:38:02 -0500

    Added initial check for match before running
    Only do “for dir” if there exists a match; changes matched dir to green
    after matched file is moved. If no matching dir exists, changes file to
    orange.

--- TOHmatcher.sh
+++ TOHmatcher.sh
@@ -10,26 +10,24 @@ shopt -s nocasematch
 FN1=$(basename "$1")
 CLEAN1="${FN1%.*}"
 
-EPNUM=$(echo "$CLEAN1" | sed -E 's/Ep\. ([0-9]+)[^0-9].*/\1/')
-
+#EPNUM=$(echo "$CLEAN1" | sed -E 's/Ep\. ([0-9]+)[^0-9].*/\1/')
 
 EXISTDIRMATCH=$(ls -d /Volumes/Storage\ Unit/^Departures/*/ | egrep -c -i -e "This Old House")
-echo "$EXISTDIRMATCH"
+#echo "$EXISTDIRMATCH"
 if [[ $EXISTDIRMATCH = 1 ]] ; then
 
 	for dir in /Volumes/Storage\ Unit/^Departures/*/
 	do
 	FNDIR=$(basename "$dir")
 	if [[ $FNDIR =~ "This Old House" ]] ; then
-		#osascript -e "tell application \"Finder\" to set label index of alias POSIX file \"$1\" to \"2\""
-		#mv "$1" "$dir"
-		#osascript -e "tell application \"Finder\" to set label index of alias POSIX file \"$1\" to \"6\""
-		#wait 20
-		#osascript -e "tell application \"Finder\" to set label index of alias POSIX file \"$dir\" to \"6\""
+		mv "$1" "$dir"
+		wait 20
+		osascript -e "tell application \"Finder\" to set label index of alias POSIX file \"$dir\" to \"6\""
 		echo "GoodRun"
 	fi
 	done
 else
 	osascript -e "tell application \"Finder\" to set label index of alias POSIX file \"$1\" to \"1\""
+fi
 
 shopt -u nocasematch
