e3d6582bb8217c8fe4f644725c6bbdc19f89aa72
NanNans <hannahpearlherman@gmail.com>
2014-12-28 14:18:36 -0500

    Fix for SickRage coming eps layout

--- forWorkflows/ComingsEpsName.sh
+++ forWorkflows/ComingsEpsName.sh
@@ -8,11 +8,11 @@ parse_name()
 {
 local NAME="$LONG"
 local NEW=""
-MATCH_COMINGEPS=$(echo "$NAME" | egrep -c -i "[0-9]{2,4}x[0-9]*")
+MATCH_COMINGEPS=$(echo "$NAME" | egrep -c -i "S[0-9]{2,4}E[0-9]*")
 MATCH_TVDB=$(echo "$NAME" | egrep -c -i "[0-9] [0-9][0-9]* :")
 
 if [[ "$MATCH_COMINGEPS" = "1" ]] ; then
-	NEW=$(echo "$NAME" | sed -E 's/(.*) ([0-9]{2,4}x[0-9]*)(.*)/\1- \2 - \3/' | sed -E 's/([^ ])-/\1 -/')
+	NEW=$(echo "$NAME" | sed -E 's/(.*)S([0-9]{2,4})E([0-9]*)(.*)/\1- \2x\3 - \4/' | sed -E 's/([^ ])-/\1 -/')
 elif [[ "$MATCH_TVDB" = "1" ]] ; then
 	NEW=$(echo "$NAME" | sed -E 's/([0-9][0-9]*) ([0-9][0-9]*) :/- \1x\2 -/')
 fi
