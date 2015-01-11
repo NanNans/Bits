1004c4546cf525f046c70baef2b3975feb333059
NanNans <hannahpearlherman@gmail.com>
2015-01-11 11:36:32 -0500

    Change to make work in Kodi Helix

--- selectbookmark.py
+++ selectbookmark.py
@@ -2,6 +2,6 @@ import xbmc, xbmcgui
 
 xbmc.executebuiltin('xbmc.ActivateWindow(videobookmarks)')
 xbmc.executebuiltin('ActivateWindowAndFocus(10125,2,0)')
-xbmc.executebuiltin('Action(Up)')
+xbmc.executebuiltin('Action(Right)')
 xbmc.executebuiltin('Action(Select)')
 xbmc.executebuiltin('xbmc.action(previousmenu)')
